ccc239bf93f823a55bbc0792c3c3b24b5e48e20b
Andrew Vorobyov <vorandrew@gmail.com>
2017-04-29 02:28:00 +0300

    Refactor

--- exchanger.go
+++ exchanger.go
@@ -37,32 +37,3 @@ type Exchanger interface {
 	NewOrder(Order) (string, error)
 	CancelOrder(string) (bool, error)
 }
-
-type exchng struct {
-	pos []Position
-	ord []Order
-}
-
-func (e *exchng) FetchOrders() (o []Order, err error) {
-	return
-}
-
-func (e *exchng) FetchPositions() (p []Position, err error) {
-	return
-}
-
-func (e *exchng) Orders() (o []Order) {
-	return e.ord
-}
-
-func (e *exchng) Positions() (p []Position) {
-	return e.pos
-}
-
-func (e *exchng) NewOrder(o Order) (ID string, err error) {
-	return
-}
-
-func (e *exchng) CancelOrder(ID string) (ok bool, err error) {
-	return
-}
--- orderManagement_test.go
+++ orderManagement_test.go
@@ -7,6 +7,35 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+type exchng struct {
+	pos []Position
+	ord []Order
+}
+
+func (e *exchng) FetchOrders() (o []Order, err error) {
+	return
+}
+
+func (e *exchng) FetchPositions() (p []Position, err error) {
+	return
+}
+
+func (e *exchng) Orders() (o []Order) {
+	return e.ord
+}
+
+func (e *exchng) Positions() (p []Position) {
+	return e.pos
+}
+
+func (e *exchng) NewOrder(o Order) (ID string, err error) {
+	return
+}
+
+func (e *exchng) CancelOrder(ID string) (ok bool, err error) {
+	return
+}
+
 var _ = Describe("OrderManagement", func() {
 
 	symbol := "BTC/USD"
